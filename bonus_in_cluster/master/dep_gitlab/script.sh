faf5ccea5dd7a8d10515dfbce2e38fa01e27bac8
AbdouBouallaga <babdelka@student.1337.ma>
2022-01-10 17:05:18 +0100

    bonus in cluster work just fine

--- bonus_in_cluster/master/dep_gitlab/script.sh
+++ bonus_in_cluster/master/dep_gitlab/script.sh
@@ -1,6 +1,8 @@
-    echo "deploying gitlab"
-    /usr/local/bin/kubectl create namespace gitlab
-    /usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/service.yaml
-    /usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/glconfig.yaml
-    /usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/deployment.yaml
-    /usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/ingress.yaml
+echo "deploying gitlab"
+/usr/local/bin/kubectl taint nodes part3bonus dedicated:NoSchedule
+/usr/local/bin/kubectl create namespace gitlab
+/usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/service.yaml
+/usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/glconfig.yaml
+/usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/ingress.yaml
+sleep 2
+/usr/local/bin/kubectl apply -f /vagrant/dep_gitlab/deployment.yaml
